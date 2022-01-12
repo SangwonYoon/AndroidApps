36a9ed44f57b09d1a134dd45a8640aa1154362e6
SangwonYoon <yoonsw0532@naver.com>
2022-01-13 00:18:07 +0900

    기본 UI 구성

---
+++ AudioRecorder/app/src/main/java/com/example/audiorecorder/State.kt
@@ -0,0 +1,10 @@
+package com.example.audiorecorder
+
+// enum class란 열거형 클래스
+// 각 열거형은 enum class의 인스턴스
+enum class State {
+    BEFORE_RECORDING,
+    ON_RECORDING,
+    AFTER_RECORDING,
+    ON_PLAYING
+}
